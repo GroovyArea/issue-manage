f3c26350657253280d74341b60eb01a6132d7406
GroovyArea <095201a@naver.com>
2023-02-12 18:33:24 +0900

    [#4] feat : 로그인 로그아웃 기능 개발

--- user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
@@ -1,10 +1,14 @@
 package com.fastcampus.userservice.controller
 
-import com.fastcampus.userservice.model.SignUpRequest
+import com.fastcampus.userservice.model.*
 import com.fastcampus.userservice.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,4 +21,18 @@ class UserController(
     suspend fun signUp(@RequestBody request: SignUpRequest) {
         userService.signUp(request)
     }
+
+    @PostMapping("/signin")
+    suspend fun singIn(@RequestBody signInRequest: SignInRequest): SignInResponse {
+        return userService.signIn(signInRequest)
+    }
+
+    @DeleteMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    suspend fun get(
+        @AuthToken token: String
+    ): MeResponse {
+        return MeResponse(userService.getByToken(token))
+    }
+
 }
